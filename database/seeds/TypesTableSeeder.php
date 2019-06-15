0c5a982da48f2ebd4edb025aab1a62e5bd904f3d
dev <fallassane656@gmail.com>
2019-06-15 12:38:48 +0000

    telechargement de yajra version 9

--- database/seeds/TypesTableSeeder.php
+++ database/seeds/TypesTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Str;
 class TypesTableSeeder extends Seeder
 {
     /**
@@ -12,5 +12,10 @@ class TypesTableSeeder extends Seeder
     public function run()
     {
         //
+        $Type1=App\Type::firstOrCreate(["name"=>"Cash"],["uuid"=>Str::uuid()]);
+        $Type2=App\Type::firstOrCreate(["name"=>"Orange Money"],["uuid"=>Str::uuid()]);
+        $Type3=App\Type::firstOrCreate(["name"=>"Visa"],["uuid"=>Str::uuid()]);
+        $Type4=App\Type::firstOrCreate(["name"=>"Cheque"],["uuid"=>Str::uuid()]);
+        $Type5=App\Type::firstOrCreate(["name"=>"Wari"],["uuid"=>Str::uuid()]);
     }
 }
